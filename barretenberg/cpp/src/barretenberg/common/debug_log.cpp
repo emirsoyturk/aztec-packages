3a56cf006bbd6a010fe807b3319b235bf19e99b3
ludamad <adam@aztecprotocol.com>
2023-12-08 21:07:52 +0000

    feat(bb): debug log

---
+++ barretenberg/cpp/src/barretenberg/common/debug_log.cpp
@@ -0,0 +1,9 @@
+#include "debug_log.hpp"
+
+void _debug_log_check_abort_condition(const std::string& log_str)
+{
+    const char* abort_cond = std::getenv("DEBUG_LOG_ABORT");
+    if (abort_cond != nullptr && log_str.find("operator") != std::string::npos) {
+        throw std::runtime_error("Abort condition met: " + log_str);
+    }
+}
